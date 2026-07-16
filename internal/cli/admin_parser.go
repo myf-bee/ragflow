00ce08fa1d101003173b08390df7fc46cbb87a77
maoyifeng <myfbee5@163.com>
2026-07-16 17:38:02 +0800

    ci test 5

--- internal/cli/admin_parser.go
+++ internal/cli/admin_parser.go
@@ -12,7 +12,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-// test ci 4
+// test ci 5
 
 package cli
 
