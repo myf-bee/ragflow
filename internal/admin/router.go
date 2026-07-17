c51fa01330b396ba12e811601023a7a678cf3dc6
maoyifeng <myfbee5@163.com>
2026-07-17 11:37:42 +0800

    test

--- internal/admin/router.go
+++ internal/admin/router.go
@@ -12,7 +12,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License. ci test 7 8
-//
+//  ci test 9
 
 package admin
 
