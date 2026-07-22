212077c161bca76c7df29905ff438df526003a8d
maoyifeng <myfbee5@163.com>
2026-07-22 15:46:32 +0800

    test token 2

--- internal/cli/admin_parser.go
+++ internal/cli/admin_parser.go
@@ -12,7 +12,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-// test ci 6 7 10 install  token
+// test ci 6 7 10 install  token 2
 
 package cli
 
